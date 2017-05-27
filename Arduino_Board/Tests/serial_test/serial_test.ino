cf1e70e1fd28d260f639676798d413f33b60734f
Naveen Chakravarthy <naveen.j_94@outlook.com>
2017-05-27 22:24:15 +0530

    Adding Arduino_Board directory to the repo
    This contains the code for arduino.

---
+++ Arduino_Board/Tests/serial_test/serial_test.ino
@@ -0,0 +1,28 @@
+String inputString = "";
+
+void setup() {
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  //Serial.println("1234");
+  //delay(100);
+}
+
+void serialEvent()
+{
+  inputString = "";
+  Serial.setTimeout(1);
+  //Serial.println("something came in");
+  while(Serial.available())
+  {
+    //char inChar = (char)Serial.read();
+    //inputString += inChar;
+    inputString = Serial.readString();
+  }
+  Serial.println(inputString);
+  Serial.flush();
+  inputString = "";
+}
+

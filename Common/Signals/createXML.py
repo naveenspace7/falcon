482c0176423b63fba45a2b5d5c44abc973358cc3
Naveen Chakravarthy <naveen.j_94@outlook.com>
2017-06-14 08:26:35 +0530

    Added ip_address and port number. Tested working.

--- Common/Signals/createXML.py
+++ Common/Signals/createXML.py
@@ -6,13 +6,16 @@ os.chdir('E:\\Falcon\\Common\\Signals')
 def create_xml():
 
     csv_file = open('E:\\Falcon\\Common\\Signals\\signals.csv')    
-
     csv_file.readline()
    
     robot = ET.Element("robot")
 
-    signal = ET.SubElement(robot,"signals")    
+    signal = ET.SubElement(robot,"signals")
+    config = ET.SubElement(robot,"config")
 
+    ET.SubElement(config,"param",name="ip_address",address="192.168.0.104",)
+    ET.SubElement(config,"param",name="port",value="2017",)
+    
     no = 0
     
     while(True):
@@ -22,21 +25,13 @@ def create_xml():
         if line=='':
             break
         
-        line = line.split(',') #list        
-        
+        line = line.split(',') #list                
         signal_no = "signal"
-
         ET.SubElement(signal,signal_no,name=line[0],address=line[1])     
 
     tree = ET.ElementTree(robot)
-
-    
-    
-    
     tree.write("signals.xml")
-
     csv_file.close()
-
     shutil.copy('E:\\Falcon\\Common\\Signals\\signals.xml','E:\\Falcon\\Common\\Utils\\signal\\src')
 
 create_xml()
